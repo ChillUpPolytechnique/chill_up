dafe37dad91b7166b46015f4d8bdf6de4355f104
Jonathan Schmutz <jonathanschmutz@CaptainFLam.eleves.polytechnique.fr>
2015-11-08 20:47:29 +0100

    ionic1

---
+++ ionic/www/js/app.js
@@ -0,0 +1,77 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+  });
+
+})
+
+.controller('HomeCtrl', function($scope, $ionicLoading, $state){
+ 
+  $scope.search=function(city){
+     $state.go('weather',{city: city})
+  }
+    
+})
+
+.controller('WeatherCtrl', function($scope, $ionicLoading, $stateParams, $http){
+
+  $scope.city=stateParams.city
+  url='http://openweathermap.org/find?q=montpellier'
+  $ionicLoading.show({
+    template:'Chargement...'
+  });
+  $http.get(url).succes(function(response){
+  $ionicLoading.hide();
+  $scope.weather=response;
+
+  })
+
+})
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+$stateProvider.state('home',{
+  url:'/home',
+  templateUrl: 'templates/home.html',
+  controller:'HomeCtrl'
+})
+
+$stateProvider.state('logout',{
+  url: '/logout',
+  templateUrl:'templates/logout.html',
+  //controller:'AboutCrl'
+})
+
+$stateProvider.state('new-wake',{
+  url:'/new-wake',
+  templateUrl:'templates/new-wake.html',
+  //controller:'New-wakeCtrl'
+
+})  
+
+
+$stateProvider.state('preferences',{
+  url:'/preferences',
+  templateUrl:'templates/preferences.html',
+  //controller:'New-wakeCtrl'
+})
+
+$urlRouterProvider.otherwise('/home')
+});
+
